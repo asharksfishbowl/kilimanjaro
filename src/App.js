e6a0a7e4b2d8c95e0d3c42ba0078600d1b82437e
Nick (Shark) Marino <nickmarino284@gmail.com>
2019-11-22 20:14:11 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -33,17 +33,17 @@ const images = [{
     src: MiguelDelCampoImg,
     title: 'Miguel Del Campo Photography',
     width: '33%',
-    height: '150px',
+    height: '250px',
   },{
     src: LeaMeyerRodarteImg,
     title: 'Lea Meyer Rodarte Photography',
     width: '33%',
-    height: '150px',
+    height: '350px',
   },{
     src: NickSharkMarinoImg,
     title: 'Nick Shark Marino Photography',
     width: '33%',
-    height: '150px',
+    height: '325px',
   },
 ];
 
