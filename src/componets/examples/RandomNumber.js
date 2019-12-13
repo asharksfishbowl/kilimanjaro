994e4d3d4ceee747364830aa680c6bc34509fbea
Nick (Shark) Marino <nickmarino284@gmail.com>
2019-12-13 17:09:50 -0600

    Update RandomNumber.js

--- src/componets/examples/RandomNumber.js
+++ src/componets/examples/RandomNumber.js
@@ -3,14 +3,12 @@ import AnimatedBg from "react-animated-bg";
 import Img from 'react-image';
 import OopseisImg from '../../assets/images/Oopseis.jpg';
 
-import { makeStyleres } from '@material-ui/core/styles';
+import { Container, Paper } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 
-const numberResult = function() {
-  let value = (Math.random() * 10);
-  setInterval(function () {
-    return value + value;
-  }, 10);
+const numberResult = function(number) {
+  let value = (Math.random() * number + 42);
   return value;
 }
 
@@ -19,23 +17,83 @@ const scaleImage = function(width, height, maxdim) {
   return [scale * width, scale * height];
 }
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    height:'1000px'
+  },
+  image:{
+    position:'relative'
+  }
+}));
+
 class RandomNumber extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 1,
+      scaleWidth:300,
+      scaleHeight:200,
+      maxdim:500,
+      fontColor:'primary'
+    };
+  };
+
   render(){
-    let scale = scaleImage(300, 200, 500);
-    let result = numberResult();
+    const classes = makeStyles();
+    let scale = scaleImage(this.state.scaleWidth, this.state.scaleHeight, this.state.maxdim);
+    let result = numberResult(this.state.count);
+
+    setTimeout(function () {
+
+      const fontColors = [
+        'primary',
+        'secondary',
+        'textPrimary',
+        'textSecondary',
+        'error'
+      ];
+
+      if (this.state.scaleWidth > 1000) {
+        this.state.scaleWidth = 250;
+      }
+
+      if (this.state.scaleHeight > 1000) {
+        this.state.scaleHeight = 100;
+      }
+
+      if (this.state.maxdim > 1000) {
+        this.state.maxdim = 300;
+      }
+
+      this.setState({
+        count: 4.20 + this.state.count++,
+        scaleWidth: Math.random() + this.state.scaleWidth++,
+        scaleHeight: Math.random() + this.state.scaleHeight++,
+        maxdim: Math.random() + this.state.maxdim++,
+        fontColor:fontColors[Math.floor(Math.random() * Math.floor(5))]
+      });
+    }.bind(this), 350)
+
+    const imageList = [
+      'url("https://images.dog.ceo/breeds/labrador/n02099712_3503.jpg")',
+      'url("https://images.dog.ceo/breeds/labrador/n02099712_5844.jpg")',
+      'url("https://images.dog.ceo/breeds/labrador/n02099712_5343.jpg")',
+      'url("https://images.dog.ceo/breeds/labrador/n02099712_7481.jpg")',
+      'url("https://images.dog.ceo/breeds/labrador/n02099712_7414.jpg")'
+    ];
 
     return(
       <AnimatedBg
-        colors={["red", "salmon", "blue"]}
-        duration={0.5}
-        delay={4}
-        timingFunction="fade-out"
-      >
-        <Typography variant="h2">
-          {result}
-        </Typography>
-        <br />
-        <Img src={OopseisImg} width={scale[0]} height={scale[1]} />
+        colors={imageList}
+        duration={10}
+        delay={0}
+        timingFunction="ease-out"
+        >
+        <Container className={classes.root} fixed>
+          <Typography variant="h2" color={this.state.fontColor}>
+            {result}
+          </Typography>
+        </Container >
       </AnimatedBg>
     )
   }
