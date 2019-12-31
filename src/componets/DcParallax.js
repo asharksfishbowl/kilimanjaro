28485785782a921c905eb128a59b0521c5f3a3b8
DFenz <dcmiguel07@gmail.com>
2019-12-30 20:53:58 -0600

    Update DcParallax.js

--- src/componets/DcParallax.js
+++ src/componets/DcParallax.js
@@ -14,6 +14,14 @@ import Fab from '@material-ui/core/Fab';
 import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 import Zoom from '@material-ui/core/Zoom';
 
+import YouTubeIcon from '@material-ui/icons/YouTube';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import Link from '@material-ui/core/Link';
+
 import Img from 'react-image';
 import AspectRatio from 'react-aspect-ratio';
 import '../assets/css/AspectRatio.css';
@@ -164,5 +172,39 @@ function DcParallax(props) {
       </div>
     )
 }
+
+<div className="App-link">
+  <IconButton className={classes.button} aria-label="YouTube">
+    <Link rel="stylesheet" href="https://www.youtube.com/channel/UCMem0Pu1NybZ8FRrvLDGkoA?view_as=subscriber">
+      <YouTubeIcon color="secondary" />
+    </Link>
+  </IconButton>
+  <IconButton className={classes.button} aria-label="Facebook">
+    <Link rel="stylesheet" href="https://www.facebook.com/asharksfishbowl/?fref=gs&dti=462102767518969&hc_location=group_dialog">
+      <FacebookIcon color="secondary" />
+    </Link>
+  </IconButton>
+  <IconButton className={classes.button} aria-label="Instagram">
+    <Link rel="stylesheet" href="https://www.instagram.com/asharksfishbowl/">
+      <InstagramIcon color="secondary" />
+    </Link>
+  </IconButton>
+  <IconButton className={classes.button} aria-label="LinkedIn">
+    <Link rel="stylesheet" href="https://www.linkedin.com/in/nick-marino-738861b0/">
+      <LinkedInIcon color="secondary" />
+    </Link>
+  </IconButton>
+  <IconButton className={classes.button} aria-label="Twitter">
+    <Link rel="stylesheet" href="https://twitter.com/asharksfishbowl">
+      <TwitterIcon color="secondary" />
+    </Link>
+  </IconButton>
+  <IconButton className={classes.button} aria-label="GitHub">
+    <Link rel="stylesheet" href="https://github.com/asharksfishbowl">
+      <GitHubIcon color="secondary" />
+    </Link>
+  </IconButton>
+</div>
+
 
 export default DcParallax;
