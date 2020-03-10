219e0be8174aba1384f77b26fe2da86165a94452
Nick (Shark) Marino <nick.marino@oxbowdms.com>
2020-03-10 00:50:26 -0500

    Tied pdf select to pdf viwer

---
+++ src/componets/preciousplastic/PdfSelect.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+
+import IntroMixWorkspace from './starterkit/0. Start/Intro - Mix Workspace.pdf';
+import LicenseAndDisclaimer from './starterkit/0. Start/License & Disclaimer .pdf';
+import Safety from './starterkit/0. Start/Safety.pdf'
+
+const paths = [
+  {key:'Intro - Mix Workspace', value:IntroMixWorkspace},
+  {key:'License & Disclaimer', value:LicenseAndDisclaimer},
+  {key:'Safety', value:Safety}
+];
+
+const pdfs = [];
+for (const [index, pdf] of paths.entries()) {
+  pdfs.push(<MenuItem value={pdf.value}>{pdf.key}</MenuItem>);
+}
+
+const useStyles = makeStyles(theme => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 300,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
+
+export default function PdfSelect(props) {
+  const {selection} = props;
+  const classes = useStyles();
+  const [pdf, setPDF] = React.useState('');
+
+  const handleChange = event => {
+    let selectedPDF = event.target.value;
+    setPDF(selectedPDF);
+  };
+
+  return (
+
+    <div>
+    <FormControl className={classes.formControl}>
+      <InputLabel id="pdf-select-autowidth-label">Precious Plastic Omaha</InputLabel>
+      <Select
+        labelId="pdf-select-autowidth-label"
+        id="pdf-select-autowidth"
+        value={props.pdf}
+        onChange={props.action}
+        autoWidth
+      >
+        <MenuItem value="">
+          <em>Select One</em>
+        </MenuItem>
+        {pdfs}
+      </Select>
+      <FormHelperText>Here our the steps</FormHelperText>
+    </FormControl>
+    </div>
+  );
+}
--- src/componets/preciousplastic/PreciousPlastic.js
+++ src/componets/preciousplastic/PreciousPlastic.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
 import { Document, Page, pdfjs } from "react-pdf";
 import './PreciousPlastic.css';
-
+import PdfSelect from "./PdfSelect.js";
 
 import Safety from "./Safety.pdf";
 import PlasticTypes from "./starterkit/1. Learn about plastic/Plastic-Types_A1.pdf";
 
 class PreciousPlastic extends Component {
-  state = {
-    numPages: null,
-    pageNumber: 1,
-    errorMessage: "Ohhh fuck batman"
+
+  constructor(props){
+    super(props)
+    this.preciousPlasticPdf = React.createRef();
+    this.loadDocument = this.loadDocument.bind(this);
+    this.state = {
+      numPages: null,
+      pageNumber: 1,
+      errorMessage: "Ohhh fudge batman",
+      pdf:''
+    }
+  }
+
+  loadDocument = pdf => {
+    let selectValue = pdf.target.value;
+    pdf = selectValue;
+    this.setState({ pdf });
   }
 
   onDocumentLoadSuccess = ({ numPages }) => {
@@ -29,19 +42,21 @@ class PreciousPlastic extends Component {
 
   render() {
     pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
-    const { pageNumber, numPages, errorMessage } = this.state;
-
+    const { pdf, numPages, errorMessage }  = this.state;
     return(
-      <Document
-        file={PlasticTypes}
-        onLoadSuccess={this.onDocumentLoadSuccess}
-        onLoadError={this.onDocumentLoadError}
-        onSourceError={this.onDocumentSourceError}
-        error={errorMessage}
-        className='Canvas-center'
-      >
-        <Page  height={600} width={400} pageNumber={1} />
-      </Document>
+      <div>
+        <PdfSelect action={this.loadDocument}/>
+        <Document
+          file={pdf}
+          onLoadSuccess={this.onDocumentLoadSuccess}
+          onLoadError={this.onDocumentLoadError}
+          onSourceError={this.onDocumentSourceError}
+          error={errorMessage}
+          className='Canvas-center'
+          >
+          <Page pageNumber={1} />
+        </Document>
+      </div>
     )
   }
 }
