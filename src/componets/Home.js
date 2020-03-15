c5dac62847f1b6ae246b4093ef8b36f69478caae
Nick (Shark) Marino <nick.marino@oxbowdms.com>
2020-03-14 20:00:48 -0500

    Update Home.js

--- src/componets/Home.js
+++ src/componets/Home.js
@@ -208,32 +208,32 @@ function Home() {
           <div className="App-link">
             <IconButton className={classes.button} aria-label="YouTube">
               <Link rel="stylesheet" href="https://www.youtube.com/channel/UCMem0Pu1NybZ8FRrvLDGkoA?view_as=subscriber">
-                <YouTubeIcon color="secondary" />
+                <YouTubeIcon color="primary" />
               </Link>
             </IconButton>
             <IconButton className={classes.button} aria-label="Facebook">
               <Link rel="stylesheet" href="https://www.facebook.com/asharksfishbowl/?fref=gs&dti=462102767518969&hc_location=group_dialog">
-                <FacebookIcon color="secondary" />
+                <FacebookIcon color="primary" />
               </Link>
             </IconButton>
             <IconButton className={classes.button} aria-label="Instagram">
               <Link rel="stylesheet" href="https://www.instagram.com/asharksfishbowl/">
-                <InstagramIcon color="secondary" />
+                <InstagramIcon color="primary" />
               </Link>
             </IconButton>
             <IconButton className={classes.button} aria-label="LinkedIn">
               <Link rel="stylesheet" href="https://www.linkedin.com/in/nick-marino-738861b0/">
-                <LinkedInIcon color="secondary" />
+                <LinkedInIcon color="primary" />
               </Link>
             </IconButton>
             <IconButton className={classes.button} aria-label="Twitter">
               <Link rel="stylesheet" href="https://twitter.com/asharksfishbowl">
-                <TwitterIcon color="secondary" />
+                <TwitterIcon color="primary" />
               </Link>
             </IconButton>
             <IconButton className={classes.button} aria-label="GitHub">
               <Link rel="stylesheet" href="https://github.com/asharksfishbowl">
-                <GitHubIcon color="secondary" />
+                <GitHubIcon color="primary" />
               </Link>
             </IconButton>
           </div>
