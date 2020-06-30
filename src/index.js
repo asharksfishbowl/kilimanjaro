33e795fa3660562cf5dde1cab49c7ceb39b7fd42
Nick Marino <nickmarino284@gmail.com>
2020-06-29 22:18:46 -0500

    Clean Up

--- src/index.js
+++ src/index.js
@@ -17,6 +17,8 @@ const firebaseConfig = {
 };
 firebase.initializeApp(firebaseConfig);
 
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
