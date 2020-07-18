eec58ed985f4d8a0a9ca081a0e7afb1bfa3b49eb
Nick Marino <nickmarino284@gmail.com>
2020-07-18 01:18:26 -0500

    Got Hello World to Work!!!! :)

--- src/componets/home/controllers/Home.js
+++ src/componets/home/controllers/Home.js
@@ -1,31 +1,15 @@
-import React from 'react';
 import firebase from '../../../firebase.js';
-import SweetAlert from 'react-bootstrap-sweetalert';
+import swal from 'sweetalert';
 
 class Home {
   helloWorld(){
     const callHelloWorld = firebase.functions.httpsCallable('helloWorld');
-
     callHelloWorld('Hello')
       .then(
-        (result) =>
-          <SweetAlert
-            success
-            onConfirm={(response) => this.onRecieveInput(response)}
-            title="We connected!!!"
-          >
-            {result}
-          </SweetAlert>
+        (result) => {swal(result.data, "We connected!", "success")}
       )
       .catch(
-        (error) =>
-          <SweetAlert
-            error
-            onConfirm={(response) => this.onRecieveInput(response)}
-            title="Oops World"
-          >
-            Error = {error.message} Error
-          </SweetAlert>
+        (error) => {swal(error.message, "We're still lost :(", "error")}
       );
   }
 }
