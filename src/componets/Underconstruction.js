a5395d62e7461c13f707b3d2a0f9020cd5a8e864
Nick Marino <nickmarino284@gmail.com>
2020-07-18 23:34:15 -0500

    trololo

--- src/componets/Underconstruction.js
+++ src/componets/Underconstruction.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { useHistory } from "react-router-dom";
 import Img from 'react-image';
 import UnderconstructionImg from '../assets/images/ASF/Underconstruction.jpg';
 
@@ -6,7 +7,9 @@ class Underconstruction extends Component {
   render() {
     return(
       <div>
-        <Img src={UnderconstructionImg} className="App-underconstruction" alt="Underconstruction" />
+        <Img src={UnderconstructionImg} className="App-underconstruction" alt="Underconstruction" onClick={trololo => { 
+          window.location.href = "https://www.youtube.com/watch?v=2Z4m4lnjxkY";
+        }}/>
       </div>
     )
   }
