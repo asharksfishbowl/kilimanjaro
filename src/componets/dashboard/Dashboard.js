0c2723105408035ad850dbbdfd977ec226480e1b
Nick (Shark) Marino <nickmarino284@gmail.com>
2020-07-25 11:05:27 -0500

    Update Dashboard.js

--- src/componets/dashboard/Dashboard.js
+++ src/componets/dashboard/Dashboard.js
@@ -91,7 +91,7 @@ function Dashboard(){
         </div>
         <Container className={classes().cardGrid} maxWidth="md">
           {/* End hero unit */}
-          <Grid container spacing={4}>
+          <Grid container spacing={4} className={animationClass}>
             {courses.map((course) => (
               <Grid item key={course.key} xs={12} sm={6} md={4}>
                 <Card className={classes().card}>
