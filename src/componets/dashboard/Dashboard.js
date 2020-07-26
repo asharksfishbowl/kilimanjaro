94043b9656137081d23c7f17f6162132fd9e3b5c
Nick Marino <nickmarino284@gmail.com>
2020-07-25 23:01:23 -0500

    #6 Cleaning it up some more

--- src/componets/dashboard/Dashboard.js
+++ src/componets/dashboard/Dashboard.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Styles from './Styles.js';
-// import DashboardController from './controllers/Dashboard.js';
+import DashboardController from './controllers/Dashboard.js';
 import AppBar from '../AppBar.js';
 import AppBottomBar from '../AppBottomBar.js';
 import Copyright from '../Copyright.js';
@@ -20,48 +20,10 @@ import IconButton from '@material-ui/core/IconButton';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
 
-// Course Images
-import SoftwareCourseImg from './assets/software.png';
-import MusicCourseImg from './assets/music.jpg';
-import PhotoCourseImg from './assets/photos.jpg';
-import ArtCourseImg from './assets/art.jpg';
-import CookingCourseImg from './assets/cooking.jpg';
-import RecycleCourseImg from './assets/recycle.jpg';
-
 function Dashboard(){
   const classes = Styles();
   const [animationClass] = useState('background-grad');
-  const courses = [{
-    key: 1,
-    image: SoftwareCourseImg,
-    link: '/SoftwareCreation',
-    description: 'Here we will dive into code and start with Web Apps and then Android & IOS Next :)'
-  },{
-    key: 2,
-    image: MusicCourseImg,
-    link: '/MusicCreation',
-    description: 'Here we will dive into my 25 year madness and passion for sound, creating a full song from the ground up'
-  },{
-    key: 3,
-    image: PhotoCourseImg,
-    link: '/VideoCreation',
-    description: 'Here we will dive into how to create cool photos, everthing from neat editing tricks to everything I have learned with a camera'
-  },{
-    key: 4,
-    image: ArtCourseImg,
-    link: '/MakerCreation',
-    description: 'Here we will dive into ways I like to relax by creating art, all kinds to! :)'
-  },{
-    key: 5,
-    image: CookingCourseImg,
-    link: '/MakerCreation',
-    description: 'Here we dive into how to create fun, creative and most of all, delicious meals :)'
-  },{
-    key: 6,
-    image: RecycleCourseImg,
-    link: '/PreciousPlastic',
-    description: 'Here we will dive into how recycle material into something new and awesome! :)'
-  }];
+  const courses = DashboardController.getCourses();
 
   return(
     <div className={animationClass}>
@@ -118,7 +80,7 @@ function Dashboard(){
                     <IconButton aria-label="share">
                       <ShareIcon />
                     </IconButton>
-                    <Button size="small" color="primary" href={course.link}>
+                    <Button size="medium" color="primary" edge="end" href={course.link}>
                       Learn More
                     </Button>
                   </CardActions>
--- src/componets/dashboard/controllers/Dashboard.js
+++ src/componets/dashboard/controllers/Dashboard.js
@@ -1,6 +1,51 @@
 import firebase from '../../../firebase.js';
 
+// Course Images
+import SoftwareCourseImg from '../assets/software.png';
+import MusicCourseImg from '../assets/music.jpg';
+import PhotoCourseImg from '../assets/photos.jpg';
+import ArtCourseImg from '../assets/art.jpg';
+import CookingCourseImg from '../assets/cooking.jpg';
+import RecycleCourseImg from '../assets/recycle.jpg';
+
+// TODO: Make this firbase driven
+const courses = [{
+  key: 1,
+  image: SoftwareCourseImg,
+  link: '/SoftwareCreation',
+  description: 'Here we will dive into code and start with Web Apps and then Android & IOS Next :)'
+},{
+  key: 2,
+  image: MusicCourseImg,
+  link: '/MusicCreation',
+  description: 'Here we will dive into my 25 year madness and passion for sound, creating a full song from the ground up'
+},{
+  key: 3,
+  image: PhotoCourseImg,
+  link: '/VideoCreation',
+  description: 'Here we will dive into how to create cool photos, everthing from neat editing tricks to everything I have learned with a camera'
+},{
+  key: 4,
+  image: ArtCourseImg,
+  link: '/MakerCreation',
+  description: 'Here we will dive into ways I like to relax by creating art, all kinds to! :)'
+},{
+  key: 5,
+  image: CookingCourseImg,
+  link: '/MakerCreation',
+  description: 'Here we dive into how to create fun, creative and most of all, delicious meals :)'
+},{
+  key: 6,
+  image: RecycleCourseImg,
+  link: '/PreciousPlastic',
+  description: 'Here we will dive into how recycle material into something new and awesome! :)'
+}];
+
 class Dashboard {
+  getCourses(){
+    return courses;
+  }
+
   helloWorld(){
     const callHelloWorld = firebase.functions.httpsCallable('helloWorld');
     callHelloWorld()
