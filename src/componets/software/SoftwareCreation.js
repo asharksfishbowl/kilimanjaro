9418f5892371dd209f0de366a75addaddaff36f2
Nick Marino <nickmarino284@gmail.com>
2020-07-30 22:33:34 -0500

    Update SoftwareCreation.js

--- src/componets/software/SoftwareCreation.js
+++ src/componets/software/SoftwareCreation.js
@@ -4,7 +4,8 @@ import AspectRatio from 'react-aspect-ratio';
 
 import Typography from '@material-ui/core/Typography';
 import TopBar from '../bars/TopBar.js';
-
+import BottomBar from '../bars/BottomBar.js';
+import Copyright from '../Copyright.js';
 
 import Shark from '../../assets/images/shark.jpg';
 import Course from './Course.js';
@@ -30,6 +31,7 @@ class SoftwareCreation extends Component {
         <AspectRatio ratio="1/4" style={{ maxWidth: '1000px' }}>
           <Img src={Shark} />
         </AspectRatio>
+        <BottomBar title=<Copyright color='secondary'/> />
       </div>
     )
   }
