623b8fe435967d0cba16f5c31598517cc6492813
Nick Marino <nickmarino284@gmail.com>
2020-08-14 01:12:36 -0500

    Update Course.js

--- src/componets/courses/Course.js
+++ src/componets/courses/Course.js
@@ -4,7 +4,6 @@ import Styles from './Styles.js';
 
 import SoftwareImg from './assets/software.png';
 
-import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
 import Card from '@material-ui/core/Card';
@@ -17,8 +16,6 @@ import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
 import DeleteIcon from '@material-ui/icons/Delete';
 import TextField from '@material-ui/core/TextField';
 import Switch from '@material-ui/core/Switch';
