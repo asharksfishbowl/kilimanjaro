77187742e7bbab8ac2a057a85e9be4544e970265
Nick Marino <nickmarino284@gmail.com>
2020-09-10 01:58:25 -0500

    Cleaning up the courses

--- src/componets/courses/Course.js
+++ src/componets/courses/Course.js
@@ -7,7 +7,9 @@ import SoftwareImg from './assets/software.png';
 
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
+import Avatar from '@material-ui/core/Avatar';
 import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
 import CardActions from '@material-ui/core/CardActions';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
@@ -19,6 +21,9 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 import TextField from '@material-ui/core/TextField';
 import Switch from '@material-ui/core/Switch';
 
@@ -26,6 +31,8 @@ function Course(props) {
   const classes = Styles();
   const [backdrop, setShowBackdrop] = useState(false);
   const [name, setName] = useState('');
+  const [date, setDate] = useState('');
+  const [description, setDescription] = useState('');
   const [visible, setVisible] = useState(false);
   const [order, setOrder] = useState('');
   const [lessons, setLessons] = useState('');
@@ -39,12 +46,20 @@ function Course(props) {
     setName(event.target.value);
   };
 
+  const onDateChange = (event) => {
+    setDate(event.target.value);
+  };
+
+  const onDescriptionChange = (event) => {
+    setDescription(event.target.value);
+  };
+
   const onOrderChange = (event) => {
     setOrder(event.target.value);
   };
 
   const onVisibleChange = (event) => {
-    setVisible(event.target.checked);
+    setVisible(event.target.value);
   };
 
   const hideBackdrop = () => {
@@ -54,6 +69,14 @@ function Course(props) {
   const showBackdrop = () => {
     setShowBackdrop(!backdrop);
   };
+
+  function resetForm(){
+    setName('');
+    setDate('');
+    setDescription('');
+    setVisible(false);
+    setOrder('');
+  }
 
   async function saveRecord() {
     showBackdrop();
@@ -83,91 +106,124 @@ function Course(props) {
   };
 
   return(
-
-    <div className={classes.root}>
-      <Container className={classes.cardGrid} maxWidth="md">
-        <Grid container spacing={4}>
-          {lessons.length ? lessons.map((lesson, key) => (
-            <Grid item key={key} xs={12} sm={6} md={4}>
-              <Card className={classes.card}>
-                <CardActionArea href="/Courses">
-                  <CardMedia
-                    className={classes.cardMedia}
-                    image={SoftwareImg}
-                    title={lesson.name}
-                  />
-                </CardActionArea>
-                <CardContent className={classes.cardContent}>
-                  <Typography>
-                    {lesson.name}
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Button size="medium" color="primary" href="/Courses">
-                    Learn More
-                  </Button>
-                  <IconButton aria-label="delete" onClick={() => { editRecord(lesson.name)}}>
-                    <EditIcon />
-                  </IconButton>
-                  <IconButton aria-label="delete" onClick={() => { deleteRecord(lesson.name)}}>
-                    <DeleteIcon />
-                  </IconButton>
-                </CardActions>
-              </Card>
-            </Grid>
-          )) : <Grid item xs={12} sm={6} md={4} /> }
-        </Grid>
-
-        <Grid container spacing={3}>
-          <Grid item xs={12}>
+    <Container className={classes.root} maxWidth="md">
+      <Grid container className={classes.cardGrid} spacing={4}>
+        {lessons.length ? lessons.map((lesson, key) => (
+          <Grid item key={key} xs={12} sm={6} md={4}>
             <Card className={classes.card}>
+              <CardHeader
+                avatar={
+                  <Avatar aria-label="recipe" className={classes.avatar}>
+                    ASF
+                  </Avatar>
+                }
+                action={
+                  <IconButton aria-label="settings">
+                  <MoreVertIcon />
+                  </IconButton>
+                }
+                title={lesson.name}
+                subheader={lesson.date}
+              />
+              <CardActionArea href="/Courses">
+                <CardMedia
+                  className={classes.cardMedia}
+                  image={SoftwareImg}
+                  title={lesson.name}
+                />
+              </CardActionArea>
               <CardContent className={classes.cardContent}>
-                <form noValidate autoComplete="off">
-                  <TextField
-                    id="name"
-                    label="Name"
-                    value={name}
-                    onChange={onNameChange}
-                    required
-                    fullWidth
-                  />
-                  <TextField
-                    id="order"
-                    label="Order"
-                    type="number"
-                    value={order}
-                    onChange={onOrderChange}
-                    fullWidth
-                  />
-                  <FormGroup row>
-                    <FormControlLabel
-                      control={
-                        <Switch
-                          checked={visible.checked}
-                          onChange={onVisibleChange}
-                          name="visible"
-                          color="primary"
-                        />
-                      }
-                      label="Visible"
-                      labelPlacement="end"
-                    />
-                  </FormGroup>
-                </form>
+                <Typography>
+                  {lesson.description}
+                </Typography>
               </CardContent>
               <CardActions>
-                <Button color="primary" onClick={saveRecord} fullWidth>
-                  Save
+                <Button size="medium" color="primary" href="/Courses">
+                  Learn More
                 </Button>
+                <IconButton aria-label="delete" onClick={() => { editRecord(lesson.name)}}>
+                  <EditIcon />
+                </IconButton>
+                <IconButton aria-label="delete" onClick={() => { deleteRecord(lesson.name)}}>
+                  <DeleteIcon />
+                </IconButton>
               </CardActions>
-
             </Card>
           </Grid>
+        )) : <Grid item xs={12} sm={6} md={4} /> }
+      </Grid>
+
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Card className={classes.card}>
+            <CardContent className={classes.cardContent}>
+              <form noValidate autoComplete="off">
+                <TextField
+                  id="name"
+                  label="Name"
+                  value={name}
+                  onChange={onNameChange}
+                  required
+                  fullWidth
+                />
+                <TextField
+                  id="date"
+                  label="Live Date"
+                  type="datetime-local"
+                  className={classes.dateField}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  value={date}
+                  onChange={onDateChange}
+                  fullWidth
+                />
+                <TextField
+                  id="description"
+                  label="Description"
+                  multiline
+                  rowsMax={4}
+                  value={description}
+                  onChange={onDescriptionChange}
+                  fullWidth
+                />
+                <TextField
+                  id="order"
+                  label="Order"
+                  type="number"
+                  value={order}
+                  onChange={onOrderChange}
+                  fullWidth
+                />
+                <FormGroup row>
+                  <FormControlLabel
+                    control={
+                      <Switch
+                        checked={visible.checked}
+                        onChange={onVisibleChange}
+                        name="visible"
+                        color="primary"
+                      />
+                    }
+                    label="Visible"
+                    labelPlacement="end"
+                  />
+                </FormGroup>
+              </form>
+            </CardContent>
+            <CardActions>
+              <Button color="primary" onClick={resetForm} fullWidth>
+                Clear
+              </Button>
+              <Button color="primary" onClick={saveRecord} fullWidth>
+                Save
+              </Button>
+            </CardActions>
+
+          </Card>
         </Grid>
-      </Container>
-
-
-    </div>
+      </Grid>
+    </Container>
   )
 }
 export default Course;
--- src/componets/courses/Styles.js
+++ src/componets/courses/Styles.js
@@ -1,4 +1,5 @@
 import { makeStyles } from '@material-ui/core/styles';
+import { red } from '@material-ui/core/colors';
 
 const Styles = () => {
   const useStyles = makeStyles(theme => ({
@@ -8,109 +9,6 @@ const Styles = () => {
       minWidth: 300,
       width: '100%',
       'z-index':1,
-    },
-    button: {
-      margin: theme.spacing(1),
-      width: '20%',
-      height: '35px',
-      fontSize: '30px'
-    },
-    iconButton: {
-      margin: theme.spacing(1)
-    },
-    input: {
-      display: 'none',
-    },
-    slider:{
-      width:200,
-      'z-index':1,
-    },
-    list:{
-      textAlign: 'center',
-      'z-index':1,
-    },
-    listCenter:{
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    image: {
-      position: 'relative',
-      height: 150,
-      [theme.breakpoints.down('xs')]: {
-        width: '100% !important', // Overrides inline-style
-        height: 100,
-      },
-      '&:hover, &$focusVisible': {
-        zIndex: 1,
-        '& $imageBackdrop': {
-          opacity: 0.15,
-        },
-        '& $imageMarked': {
-          opacity: 0,
-        },
-        '& $imageTitle': {
-          border: '4px solid currentColor',
-        },
-      },
-    },
-    focusVisible: {},
-    imageButton: {
-      position: 'absolute',
-      left: 0,
-      right: 0,
-      top: 0,
-      bottom: 0,
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      color: theme.palette.common.white,
-    },
-    imageSrc: {
-      position: 'absolute',
-      left: 0,
-      right: 0,
-      top: 0,
-      bottom: 0,
-      backgroundSize: 'cover',
-      backgroundPosition: 'center 40%',
-      height: '100%',
-      width: '100%'
-    },
-    imageBackdrop: {
-      position: 'absolute',
-      left: 0,
-      right: 0,
-      top: 0,
-      bottom: 0,
-      backgroundColor: theme.palette.common.black,
-      opacity: 0.4,
-      transition: theme.transitions.create('opacity'),
-    },
-    imageTitle: {
-      position: 'relative',
-      padding: `${theme.spacing(2)}px ${theme.spacing(4)}px ${theme.spacing(1) + 6}px`,
-    },
-    imageMarked: {
-      height: 3,
-      width: 18,
-      backgroundColor: theme.palette.common.white,
-      position: 'absolute',
-      bottom: -2,
-      left: 'calc(50% - 9px)',
-      transition: theme.transitions.create('opacity'),
-    },
-    icon: {
-      marginRight: theme.spacing(2),
-    },
-    heroContent: {
-      backgroundColor: theme.palette.background.paper,
-      padding: theme.spacing(8, 0, 6),
-    },
-    heroButtons: {
-      marginTop: theme.spacing(4),
-      marginBottom: theme.spacing(2),
     },
     cardGrid: {
       paddingTop: theme.spacing(8),
@@ -127,16 +25,15 @@ const Styles = () => {
     cardContent: {
       flexGrow: 1,
     },
-    footer: {
-      backgroundColor: theme.palette.background.paper,
-      padding: theme.spacing(6),
+    avatar: {
+      backgroundColor: red[500],
     },
-    backdrop: {
-      zIndex: theme.zIndex.drawer + 1,
-      color: '#fff',
+    dateField: {
+      marginTop: theme.spacing(2),
+      marginBottom: theme.spacing(0.5),
     },
   }))
-  return useStyles;
+  return useStyles();
 };
 
 export default Styles;
