52103fd2d018cc444037dae66614864a2c1a3637
Nick Marino <nickmarino284@gmail.com>
2020-10-04 09:59:33 -0500

    Update Contact.js

--- src/components/contact/controllers/Contact.js
+++ src/components/contact/controllers/Contact.js
@@ -3,7 +3,6 @@ import firebase from '../../../firebase.js';
 class Contact {
   async sendMessage(message) {
     try {
-      debugger;
       const sendMail = firebase.functions.httpsCallable('sendMail');
       const record = await sendMail(message);
       return record;
